286195efcff101be28a7b1292f2d69cf823a3a0a
Alvin Abaho <alvinabaho@gmail.com>
2019-03-17 18:04:42 +0300

    Adds dev and test configuration to mongoDB ... initially want to use separate dbs in order not to have the tests overwrite the main db

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const express = require('express'),
     config = require('config'),
     port = 8080;
 
+// -------------database--------------
+// db connection
+mongoose.connect(config.DBHost, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
