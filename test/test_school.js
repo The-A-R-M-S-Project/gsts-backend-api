99a861fe3fee9fa7f9fc020cd520a58853b60637
Alvin Abaho <alvinabaho@gmail.com>
2019-03-17 18:49:54 +0300

    Adds test for GET to find all schools ... Note that we empty the db in order to start each test with a clean db

---
+++ test/test_school.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const School = require('../models/schools');
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+// Parent block
+describe('Books', () => {
+    beforeEach((done) => { // Before each test, empty the database
+        School.deleteMany({}, (err) => {
+            done();
+        });
+    });
+
+    /*
+    * /GET route test
+    */
+    describe('/GET school', () => {
+        it('it should GET all the schools', (done) => {
+            chai.request(server)
+                .get('/school')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
+    });
+
+});
