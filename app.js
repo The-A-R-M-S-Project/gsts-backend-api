693bc0943e04c66d6052474abbe9c2c1f19d5257
Alvin Abaho <alvinabaho@gmail.com>
2019-03-17 18:16:30 +0300

    Uses morgan 'combined' loggin to  output Apache style LOGs

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const express = require('express'),
     port = 8080;
 
 // -------------database--------------
-// db connection
 mongoose.connect(config.DBHost, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+// -------------logs------------------
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+    app.use(morgan('combined'));
+}
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
