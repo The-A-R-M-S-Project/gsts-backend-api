626d6487fb43b74a539a6c47bc73affdf9ef474b
Alvin Abaho <alvinabaho@gmail.com>
2019-07-01 22:31:52 +0300

    [DeprecationWarning] adds 'create Index' to mongoose connection options to avoid useing deprecated collection.ensureIndex

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let port = process.env.PORT || 8080;
 
 // -------------database--------------
 let uri = process.env.DATABASEURL || config.DBHost;
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
