3c596f451a52b0e530e142bac6acccc95cf619aa
Alvin Abaho <alvinabaho@gmail.com>
2019-07-12 16:14:08 +0300

    Handles errors outside express like Unhandled Promise rejections ... gracefully shutsdown server and exits process in this event

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = require('./app');
 // -------------database--------------
 let DB =
   process.env.NODE_ENV === 'production'
-    ? process.env.DATABASE
+    ? process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
     : process.env.DATABASE_LOCAL;
 if (process.env.NODE_ENV === 'test') DB = process.env.DATABASE_TEST;
 
@@ -20,8 +20,16 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 module.exports = app;
