499719883d640501c614bfa6da794950528d85b7
Alvin Abaho <alvinabaho@gmail.com>
2019-07-12 15:42:09 +0300

    Handles Invalid mongoDB id error -> Cast Error in production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -42,7 +49,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err }; // do not overide the err. Destructure into new var
+    console.log(`-> error ${error}`);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   } else if (process.env.NODE_ENV === 'test') {
     sendErrorTest(err, res);
   }
