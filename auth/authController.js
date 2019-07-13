f078e9880c64b8ad7beec543d9f0ead9e9154833
Alvin Abaho <alvinabaho@gmail.com>
2019-07-13 16:10:36 +0300

    sends new user a token to signIn on signUp

--- auth/authController.js
+++ auth/authController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 class AuthController {
   constructor(User) {
     this.User = User;
@@ -6,9 +8,15 @@ class AuthController {
   signup() {
     return async (req, res, next) => {
       const user = await this.User.create(req.body);
-      res
-        .status(201)
-        .json({ user, message: `${user.role} successfully added!` });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '30m',
+        issuer: 'gsts.cedat.mak.ac.ug'
+      });
+      res.status(201).json({
+        message: `${user.role} successfully added!`,
+        token,
+        user
+      });
     };
   }
 }
