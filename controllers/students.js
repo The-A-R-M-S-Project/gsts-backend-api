997c61ce081bc93e5b357424dbfd532074605b23
Alvin Abaho <alvinabaho@gmail.com>
2019-07-13 23:42:30 +0300

    Adds updateMe route to allow logged in users update their data

--- controllers/students.js
+++ controllers/students.js
@@ -2,7 +2,49 @@ const Student = require('../models/students');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 module.exports = {
+  updateMe: catchAsync(async (req, res, next) => {
+    // 1) Create error if student POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'This route is not for password updates. Please use /updatePassword.',
+          400
+        )
+      );
+    }
+
+    // 2) Filtered out unwanted fields names that are not allowed to be updated like role and only allow the following
+    const filteredBody = filterObj(
+      req.body,
+      'firstName',
+      'lastName',
+      'email',
+      'phoneNumber',
+      'photo',
+      'program'
+    );
+
+    // 3) Update Student document
+    const student = await Student.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { student }
+    });
+  }),
+
   addStudent: catchAsync(async (req, res, next) => {
     const student = await Student.create(req.body);
 
