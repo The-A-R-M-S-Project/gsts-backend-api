98f6c53e062dec08237b08275e2921a1557b1df3
Alvin Abaho <alvinabaho@gmail.com>
2019-07-13 17:38:57 +0300

    adds protect method to global authController

--- auth/authController.js
+++ auth/authController.js
@@ -52,6 +52,37 @@ class AuthController {
       });
     };
   }
+
+  protect() {
+    return async (req, res, next) => {
+      // 1) Getting token and check of it's there
+      let token;
+      if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+      }
+
+      if (!token) {
+        return next(
+          new AppError('You are not logged in! Please log in to get access.', 401)
+        );
+      }
+
+      // 2) Verification token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // 3) Check if user still exists
+      const currentUser = await this.User.findById(decoded.id);
+      if (!currentUser) {
+        return next(
+          new AppError('The user belonging to this token does no longer exist.', 401)
+        );
+      }
+
+      // GRANT ACCESS TO PROTECTED ROUTE
+      req.user = currentUser;
+      next();
+    };
+  }
 }
 
 module.exports = AuthController;
