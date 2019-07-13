3ad360088b0b503222fb74793ee80560a3ef84be
Alvin Abaho <alvinabaho@gmail.com>
2019-07-13 15:32:12 +0300

    Allows process to run in Uncaught Exception in 'test' environment

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
+if (process.env.NODE_ENV !== 'test') {
+  process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+}
 
 dotenv.config({ path: './.env' });
 const app = require('./app');
