4e92cf26e3fec869ac7f3204a0987fcbb40d2ab9
Alvin Abaho <alvinabaho@gmail.com>
2019-07-13 22:34:26 +0300

    Configures HTTPOnly Cookie to expire after 30 mins of issuing jwt

--- auth/authController.js
+++ auth/authController.js
@@ -13,13 +13,12 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  //send Token via HttpOnly cookie
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60000),
     httpOnly: true
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // only for SSL in production
 
   res.cookie('jwt', token, cookieOptions);
 
