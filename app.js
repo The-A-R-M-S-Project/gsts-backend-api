2075b54dd8231535c12ba8230ccb17c7d5e412e5
Alvin Abaho <alvinabaho@gmail.com>
2019-07-14 08:21:55 +0300

    [SECURITY] Data sanitization against NoSQL query injection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 
 const AppError = require('./utils/appError');
@@ -20,6 +21,9 @@ const app = express();
 
 // Set security HTTP headers
 app.use(helmet());
+
+// ------------ ENABLE CORS FOR ALL ORIGINS -------
+app.use(cors());
 
 // Limit requests from same API
 const limiter = rateLimit({
@@ -41,8 +45,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-// ------------ ENABLE CORS FOR ALL ORIGINS -------
-app.use(cors());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 // ---------------ROUTES ---------------------
 app.get('/', (req, res) =>
