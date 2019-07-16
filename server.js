77a795854a95a9e8ac0a1da905f735d1d11bcec4
Alvin Abaho <alvinabaho@gmail.com>
2019-07-16 23:59:44 +0300

    refactors student tests to consume promises using async/await

--- server.js
+++ server.js
@@ -31,13 +31,14 @@ const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
+if (process.env.NODE_ENV !== 'test') {
+  process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
   });
-});
+}
 
 module.exports = app;
--- test/test_students.js
+++ test/test_students.js
@@ -1,19 +1,23 @@
 process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+
 const Student = require('../models/students');
 const Program = require('../models/programs');
 const server = require('../server');
+const generators = require('./generators');
 
 const should = chai.should(); // no need to repeat this line in other tests
 chai.use(chaiHttp);
+const client = chai.request.agent(server);
 
-describe('Students', () => {
+describe.only('Students', () => {
   beforeEach(done => {
     Student.deleteMany({}, () => {});
     Program.deleteMany({}, () => {});
     done();
   });
+
   afterEach(done => {
     Student.deleteMany({}, () => {});
     Program.deleteMany({}, () => {});
@@ -21,297 +25,191 @@ describe('Students', () => {
   });
 
   describe('/GET /api/program/:id/student', () => {
-    it('Should GET all the students in a particular program', done => {
-      const program = new Program({ name: 'Master of Architecture' });
-      const student = new Student({
-        bioData: {
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      program.save((err, savedProgram) => {
-        student.save((err, savedStudent) => {
-          Student.findById(savedStudent._id, (err, foundStudent) => {
-            foundStudent.program = savedProgram;
-            foundStudent.save((err, std) => {
-              savedProgram.students.push(std);
-              savedProgram.save((err, crs) => {
-                chai
-                  .request(server)
-                  .get(`/api/program/${crs._id}/student`)
-                  .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    res.body.length.should.not.be.eql(0);
-                    res.body[0].should.have.property('_id');
-                    res.body[0].bioData.should.have
-                      .property('firstName')
-                      .eq('John');
-                    res.body[0].bioData.should.have
-                      .property('lastName')
-                      .eq('Doe');
-                    res.body[0].bioData.should.have
-                      .property('email')
-                      .eq('test@cedat.mak.ac.ug');
-                    res.body[0].bioData.should.have
-                      .property('phoneNumber')
-                      .eq('12345');
-                    res.body[0].should.have
-                      .property('program')
-                      .eq(`${crs._id}`);
-                    done();
-                  });
-              });
-            });
-          });
+    it('Should GET all the students in a particular program', async () => {
+      const program = await Program.create({ name: 'Master of Architecture' });
+      const student = await Student.create(generators.newStudent);
+      student.program = program._id;
+      program.students.push(student._id);
+
+      await program.save();
+      await student.save({ validateBeforeSave: false });
+
+      const responsePromise = new Promise((resolve, reject) => {
+        client.get(`/api/program/${program._id}/student`).then(res => {
+          resolve(res);
         });
       });
+      const res = await responsePromise;
+      res.should.have.status(200);
+      res.body.should.be.a('array');
+      res.body.length.should.not.be.eql(0);
+      res.body[0].should.have.property('_id');
+      res.body[0].should.have.property('firstName').eq('John');
+      res.body[0].should.have.property('lastName').eq('Doe');
+      res.body[0].should.have.property('email').eq('test@cedat.mak.ac.ug');
+      res.body[0].should.have.property('phoneNumber').eq('+256772123456');
+      res.body[0].should.have.property('program').eq(`${program._id}`);
     });
   });
 
   describe('/GET /api/student/:id', () => {
-    it('should GET a particular student given their ID with Authorization token', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      let token;
-      // todo: Refactor this to use a async/await and eliminate occasional callback hell!
-      // todo: Might want to refactor other tests too with chai-http promises
-      student.save(() => {
-        const client = chai.request.agent(server);
+    it('should GET a particular student when logged in', async () => {
+      const student = await Student.create(generators.newStudent);
+      const { email, password } = generators.newStudent;
+
+      const loginPromise = new Promise((resolve, reject) => {
         client
           .post(`/api/student/login`)
           .send({
-            bioData: {
-              email: 'test@cedat.mak.ac.ug'
-            },
-            password: 'testPassword'
+            email,
+            password
           })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql(true);
-            res.body.should.have.property('token');
-            ({ token } = res.body);
-            return client
-              .get(`/api/student/${student._id}/`)
-              .set('Authorization', `bearer ${token}`)
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('_id').eq(`${student._id}`);
-                res.body.bioData.should.have.property('firstName').eq('John');
-                res.body.bioData.should.have.property('lastName').eq('Doe');
-                res.body.bioData.should.have
-                  .property('email')
-                  .eq('test@cedat.mak.ac.ug');
-                res.body.bioData.should.have
-                  .property('phoneNumber')
-                  .eq('12345');
-                done();
-              });
+          .then(res => {
+            resolve(res);
           });
       });
-    });
+      const loginResponse = await loginPromise;
+      const { token } = loginResponse.body;
 
-    it('should require Authorization to GET student details', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      student.save(() => {
-        const client = chai.request.agent(server);
+      const responsePromise = new Promise((resolve, reject) => {
         client
-          .post(`/api/student/login`)
-          .send({
-            bioData: {
-              email: 'test@cedat.mak.ac.ug'
-            },
-            password: 'testPassword'
-          })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql(true);
-            res.body.should.have.property('token');
-            return client
-              .get(`/api/student/${student._id}/`)
-              .end((err, res) => {
-                res.should.have.status(401);
-                res.body.should.be.a('object');
-                res.body.should.have
-                  .property('error')
-                  .eq('Authentication error. Token required.');
-                done();
-              });
+          .get(`/api/student/${student._id}/`)
+          .set('Authorization', `Bearer ${token}`)
+          .then(res => {
+            resolve(res);
           });
       });
+      const res = await responsePromise;
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('_id').eq(`${student._id}`);
+      res.body.should.have.property('role').eq(`${student.role}`);
+      res.body.should.have.property('firstName').eq(student.firstName);
+      res.body.should.have.property('lastName').eq(student.lastName);
+      res.body.should.have.property('email').eq(student.email);
+      res.body.should.have.property('phoneNumber').eq(student.phoneNumber);
     });
   });
 
-  describe('/POST /api/student/', () => {
-    it('should successfully add a student to the database', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      chai
-        .request(server)
-        .post(`/api/student`)
-        .send(student)
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have
-            .property('message')
-            .eql('Student successfully added!');
-          res.body.student.should.have.property('_id');
-          res.body.student.bioData.should.have.property('firstName').eq('Jane');
-          res.body.student.bioData.should.have.property('lastName').eq('Doe');
-          res.body.student.bioData.should.have
-            .property('email')
-            .eq('test@cedat.mak.ac.ug');
-          res.body.student.bioData.should.have
-            .property('phoneNumber')
-            .eq('12345');
-          done();
-        });
-    });
-  });
+  // TODO: Refactor this test to only pass with Admin credentials
+  // describe('/POST /api/student/', () => {
+  //   it('should successfully add a student to the database', done => {
+  //     const student = new Student({
+  //       bioData: {
+  //         firstName: 'Jane',
+  //         lastName: 'Doe',
+  //         email: 'test@cedat.mak.ac.ug',
+  //         phoneNumber: '12345'
+  //       },
+  //       password: 'testPassword'
+  //     });
+  //     chai
+  //       .request(server)
+  //       .post(`/api/student`)
+  //       .send(student)
+  //       .end((err, res) => {
+  //         res.should.have.status(201);
+  //         res.body.should.be.a('object');
+  //         res.body.should.have.property('message').eql('Student successfully added!');
+  //         res.body.student.should.have.property('_id');
+  //         res.body.student.should.have.property('firstName').eq('Jane');
+  //         res.body.student.should.have.property('lastName').eq('Doe');
+  //         res.body.student.should.have
+  //           .property('email')
+  //           .eq('test@cedat.mak.ac.ug');
+  //         res.body.student.bioData.should.have.property('phoneNumber').eq('12345');
+  //         done();
+  //       });
+  //   });
+  // });
 
   describe('/POST /api/student/login', () => {
-    it('should successfully login with correct credentials', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'Joseph',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      student.save(() => {
-        chai
-          .request(server)
+    it('should successfully login with correct credentials', async () => {
+      const student = await Student.create(generators.newStudent);
+      const { email, password } = generators.newStudent;
+
+      const responsePromise = new Promise((resolve, reject) => {
+        client
           .post(`/api/student/login`)
           .send({
-            bioData: {
-              email: 'test@cedat.mak.ac.ug'
-            },
-            password: 'testPassword'
+            email,
+            password
           })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql(true);
-            res.body.should.have.property('token');
-            done();
+          .then(res => {
+            resolve(res);
           });
       });
+      const res = await responsePromise;
+      res.should.have.status(201);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('success');
+      res.body.should.have.property('token');
+      res.body.data.user.should.have.property('firstName').eq(student.firstName);
+      res.body.data.user.should.have.property('lastName').eq(student.lastName);
+      res.body.data.user.should.have.property('email').eq(student.email);
+      res.body.data.user.should.have.property('phoneNumber').eq(student.phoneNumber);
     });
 
-    it('should return Authentication error for incorrect credentials', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      student.save(() => {
-        chai
-          .request(server)
+    it('should return Authentication error for incorrect credentials', async () => {
+      await Student.create(generators.newStudent);
+      const { email } = generators.newStudent;
+
+      const responsePromise = new Promise((resolve, reject) => {
+        client
           .post(`/api/student/login`)
           .send({
-            bioData: {
-              email: 'test@cedat.mak.ac.ug'
-            },
+            email,
             password: 'wrongTestPassword'
           })
-          .end((err, res) => {
-            res.should.have.status(401);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql(false);
-            res.body.should.have.property('error').eql('Authentication error');
-            done();
+          .then(res => {
+            resolve(res);
           });
       });
+      const res = await responsePromise;
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('fail');
+      res.body.should.have.property('message').eql('Incorrect email or password');
     });
   });
 
   describe('/PATCH /api/student/:id', () => {
-    it('should update student information given the id', done => {
-      const student = new Student({
-        bioData: {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'test@cedat.mak.ac.ug',
-          phoneNumber: '12345'
-        },
-        password: 'testPassword'
-      });
-      let token;
-      student.save(() => {
-        const client = chai.request.agent(server);
+    it('should update student information given the id', async () => {
+      const student = await Student.create(generators.newStudent);
+      const { email, password } = generators.newStudent;
+
+      const loginPromise = new Promise((resolve, reject) => {
         client
           .post(`/api/student/login`)
           .send({
-            bioData: {
-              email: 'test@cedat.mak.ac.ug'
-            },
-            password: 'testPassword'
+            email,
+            password
           })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('success').eql(true);
-            res.body.should.have.property('token');
-            ({ token } = res.body);
-            return client
-              .patch(`/api/student/${student._id}/`)
-              .set('Authorization', `bearer ${token}`)
-              .send({
-                bioData: {
-                  firstName: 'Jane',
-                  lastName: 'Doe',
-                  email: 'test@cedat.mak.ac.ug',
-                  phoneNumber: '54321'
-                }
-              })
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have
-                  .property('message')
-                  .eql('Student information updated!');
-                res.body.student.bioData.should.have
-                  .property('phoneNumber')
-                  .eql('54321');
-                done();
-              });
+          .then(res => {
+            resolve(res);
           });
       });
+      const loginResponse = await loginPromise;
+      const { token } = loginResponse.body;
+
+      const responsePromise = new Promise((resolve, reject) => {
+        client
+          .patch(`/api/student/updateMe/`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({ phoneNumber: '+256783123456' })
+          .then(res => {
+            resolve(res);
+          });
+      });
+
+      const res = await responsePromise;
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('success');
+      res.body.data.student.should.have.property('role').eq(`${student.role}`);
+      res.body.data.student.should.have.property('firstName').eq(student.firstName);
+      res.body.data.student.should.have.property('lastName').eq(student.lastName);
+      res.body.data.student.should.have.property('email').eq(student.email);
+      res.body.data.student.should.have.property('phoneNumber').eq('+256783123456');
     });
   });
 });
