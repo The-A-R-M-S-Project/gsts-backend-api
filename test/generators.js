5643e73e0b286f3a58ad08a760742440334d4746
Alvin Abaho <alvinabaho@gmail.com>
2019-07-16 23:57:38 +0300

    Adds test generators

---
+++ test/generators.js
@@ -0,0 +1,8 @@
+exports.newStudent = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'test@cedat.mak.ac.ug',
+  password: '12345678',
+  passwordConfirm: '12345678',
+  phoneNumber: '+256772123456'
+};
