58a8e6e48a0d49e67c8eacff69faeb5034b2d346
Alvin Abaho <alvinabaho@gmail.com>
2019-07-17 09:42:32 +0300

    Adds new and improved data seeder
    ... Run in terminal using ```node import-dev-data.js --import/--delete```

---
+++ dev-data/import-dev-data.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Schools = require('../models/schools');
+const Departments = require('../models/departments');
+const Programs = require('../models/programs');
+const Students = require('../models/students');
+
+dotenv.config();
+
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+function getDataPromise(fileName, type) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(fileName, type, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+let schools = getDataPromise(`${__dirname}/schools.json`, 'utf-8');
+let departments = getDataPromise(`${__dirname}/departments.json`, 'utf-8');
+let programs = getDataPromise(`${__dirname}/programs.json`, 'utf-8');
+let students = getDataPromise(`${__dirname}/students.json`, 'utf-8');
+
+const importDataFromModel = async (Model, data) => {
+  try {
+    await Model.create(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const deleteDataFromModel = async Model => {
+  try {
+    await Model.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  [schools, departments, programs, students] = await Promise.all([
+    schools,
+    departments,
+    programs,
+    students
+  ]);
+
+  await importDataFromModel(Schools, JSON.parse(schools));
+  await importDataFromModel(Departments, JSON.parse(departments));
+  await importDataFromModel(Programs, JSON.parse(programs));
+  await importDataFromModel(Students, JSON.parse(students));
+  console.log('Data successfully imported!');
+  process.exit();
+};
+
+// DELETE DATA FROM DB
+const deleteData = async () => {
+  await deleteDataFromModel(Schools);
+  await deleteDataFromModel(Departments);
+  await deleteDataFromModel(Programs);
+  await deleteDataFromModel(Students);
+  console.log('Data successfully deleted!');
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
