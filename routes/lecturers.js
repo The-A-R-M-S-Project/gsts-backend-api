f875e61c45ad18c98b94c1ba299d849a627433c2
Alvin Abaho <alvinabaho@gmail.com>
2019-07-17 14:20:31 +0300

    Adds admin auth and improved controllers

--- routes/lecturers.js
+++ routes/lecturers.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const controller = require('../controllers/lecturers');
+const authController = require('../auth/lecturerAuth');
 
 const router = express.Router();
 
-router.route('/').get(controller.getAllLecturers);
-router.route('/:id').get(controller.getLecturer);
+router.post('/signup', authController.signup());
+router.post('/login', authController.login());
+router.get('/logout', authController.logout());
+router.post('/forgotPassword', authController.forgotPassword());
+router.patch('/resetPassword/:token', authController.resetPassword());
+
+router.use(authController.protect());
+
+router.patch('/updateMe', controller.updateMe);
+router.delete('/deactivateMe', controller.deactivateMe);
+router.get('/me', authController.getMe(), controller.getLecturer);
+router.patch('/updatePassword', authController.updatePassword());
+
+router
+  .route('/')
+  .get(
+    authController.restrictTo('admin', 'principal', 'dean'),
+    controller.getAllLecturers
+  )
+  .post(authController.restrictTo('admin'), controller.addLecturer);
+router
+  .route('/:id')
+  .get(authController.restrictTo('admin'), controller.getLecturer)
+  .patch(authController.restrictTo('admin', 'dean'), controller.updateLecturer);
 
 module.exports = router;
