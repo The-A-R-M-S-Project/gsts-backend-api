beb05abc1f5940fe6ac5f729ed4a1f8af2966d0d
calvinkaregyeya <calvinkaregyeya@gmail.com>
2019-07-27 22:30:46 +0300

    Modifies departments controller getAllProgramsFromDepartment route to fetch all programs that match a department id from the program model

--- controllers/departments.js
+++ controllers/departments.js
@@ -1,4 +1,5 @@
 const Department = require('../models/departments');
+const Program = require('../models/programs');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
@@ -24,13 +25,11 @@ module.exports = {
   }),
 
   getAllProgramsFromDepartment: async (req, res, next) => {
-    const department = await Department.findById(req.params.id)
-      .populate({ path: 'programs', select: 'name _id' })
-      .sort({ name: 1 });
+    const programs = await Program.find({ department: req.params.id });
 
-    if (!department) {
+    if (!programs) {
       return next(new AppError('No department found with that id', 404));
     }
-    res.status(200).json({ department, programs: department.programs });
+    res.status(200).json({ programs });
   }
 };
