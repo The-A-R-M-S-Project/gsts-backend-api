6d5df5ed659301a8412245565f83d0a7a5026aae
calvinkaregyeya <calvinkaregyeya@gmail.com>
2019-07-29 14:22:10 +0300

    Removes populate programs path from department controllers

--- controllers/departments.js
+++ controllers/departments.js
@@ -5,18 +5,13 @@ const catchAsync = require('./../utils/catchAsync');
 
 module.exports = {
   getAllDepartments: catchAsync(async (req, res, next) => {
-    const departments = await Department.find({})
-      .populate({ path: 'programs', select: 'name -_id' })
-      .sort({ name: 1 });
+    const departments = await Department.find({}).sort({ name: 1 });
 
     res.status(200).json(departments);
   }),
 
   getDepartment: catchAsync(async (req, res, next) => {
-    const department = await Department.findById(req.params.id).populate({
-      path: 'programs',
-      select: 'name -_id'
-    });
+    const department = await Department.findById(req.params.id);
 
     if (!department) {
       return next(new AppError('No department found with that id', 404));
@@ -24,12 +19,12 @@ module.exports = {
     res.status(200).json(department);
   }),
 
-  getAllProgramsFromDepartment: async (req, res, next) => {
+  getAllProgramsFromDepartment: catchAsync(async (req, res, next) => {
     const programs = await Program.find({ department: req.params.id });
 
     if (!programs) {
       return next(new AppError('No department found with that id', 404));
     }
     res.status(200).json({ programs });
-  }
+  })
 };
