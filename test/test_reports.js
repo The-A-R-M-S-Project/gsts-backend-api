1830bb01658256098a36b2b7603c976cabe6a4be
Alvin Abaho <alvinabaho@gmail.com>
2019-09-27 23:04:00 +0300

    refactors report tests to include new auth constraints

--- test/test_reports.js
+++ test/test_reports.js
@@ -4,6 +4,7 @@ const chaiHttp = require('chai-http');
 
 const Report = require('../models/reports');
 const Student = require('../models/students');
+const Staff = require('../models/staff');
 const server = require('../server');
 const generators = require('./generators');
 
@@ -14,54 +15,83 @@ describe('Reports', () => {
   beforeEach(done => {
     Report.deleteMany({}, () => {});
     Student.deleteMany({}, () => {});
+    Staff.deleteMany({}, () => {});
     done();
   });
   afterEach(done => {
     Report.deleteMany({}, () => {});
     Student.deleteMany({}, () => {});
+    Staff.deleteMany({}, () => {});
     done();
   });
 
   describe('/GET /api/report', () => {
-    it('Should get all the reports', async () => {
-      const student = await Student.create(generators.newStudent);
-      const report = new Report(generators.newReport);
-      report.student = student._id;
+    it('Only Elevated staff should get all the reports', async () => {
+      await Staff.create(generators.newPrincipal);
+      const { email, password } = generators.newPrincipal;
 
-      await report.save();
-
-      const requestPromise = new Promise((resolve, reject) => {
-        client.get('/api/report').then(res => {
-          resolve(res);
-        });
+      const loginPromise = new Promise((resolve, reject) => {
+        client
+          .post('/api/staff/login')
+          .send({ email, password })
+          .then(res => {
+            resolve(res);
+          });
       });
-      const res = await requestPromise;
-      res.should.have.status(200);
-      res.body.should.be.a('array');
-      res.body.should.not.be.eql(0);
-      res.body[0].should.have.property('reportStatus');
-      res.body[0].should.have.property('reportStatus');
-      res.body[0].should.have.property('student');
-    });
 
-    it('Should be able to add a report', async () => {
-      const student = new Report(generators.newStudent);
-      const report = new Report(generators.newReport);
-      report.student = student._id;
+      const loginResponse = await loginPromise;
+      const { token } = loginResponse.body;
 
       const requestPromise = new Promise((resolve, reject) => {
         client
-          .post('/api/report')
-          .send(report)
+          .get('/api/report')
+          .set('Authorization', `Bearer ${token}`)
           .then(res => {
             resolve(res);
           });
       });
       const res = await requestPromise;
+      res.should.have.status(200);
+      res.body.should.be.a('array');
+      res.body.length.should.eql(0);
+    });
+  });
+
+  describe('/api/student/report', () => {
+    it('only student should be able to add a report', async () => {
+      const student = await Student.create(generators.newStudent);
+      const { email, password } = generators.newStudent;
+
+      const loginPromise = new Promise((resolve, reject) => {
+        client
+          .post(`/api/student/login`)
+          .send({
+            email,
+            password
+          })
+          .then(res => {
+            resolve(res);
+          });
+      });
+      const loginResponse = await loginPromise;
+      const { token } = loginResponse.body;
+
+      const requestPromise = new Promise((resolve, reject) => {
+        client
+          .post(`/api/student/report`)
+          .set('Authorization', `Bearer ${token}`)
+          .send({ reportUrl: 'New Report Over here' })
+          .then(res => {
+            resolve(res);
+          });
+      });
+
+      const res = await requestPromise;
       res.should.have.status(201);
-      res.body.should.be.an('object');
-      res.body.should.have.property('message');
-      res.body.should.have.property('report');
+      res.body.should.be.a('object');
+      res.body.report.should.have.property('reportStatus').eq('notSubmitted');
+      res.body.report.should.have.property('reportUrl').eq('New Report Over here');
+      res.body.report.should.have.property('student').eq(`${student._id}`);
     });
   });
 });
