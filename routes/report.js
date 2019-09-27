4708ecb3ed412441241ed3e6d77a13ac1fb2f353
Alvin Abaho <alvinabaho@gmail.com>
2019-09-27 21:59:17 +0300

    restricts All reports route to staff

--- routes/report.js
+++ routes/report.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const controller = require('../controllers/report');
+const staffAuth = require('../auth/staffAuth');
+const AuthProtector = require('../auth/authProtector');
 
 const router = express.Router();
 
+router.use(AuthProtector());
+router.use(staffAuth.restrictTo('admin', 'principal', 'dean'));
 router.route('/').get(controller.getAllReports);
 
 module.exports = router;
