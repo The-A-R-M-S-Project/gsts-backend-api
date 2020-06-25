5872ce4ccc7af7a75e1083e3e73195f21e6f0ac8
Alvin Abaho <abahoalvin@gmail.com>
2020-06-25 20:39:37 +0300

    copies env variables using scp

---
+++ scripts/copy-env.sh
@@ -0,0 +1,17 @@
+echo 'creating .env file ...'
+echo PORT=$PORT >> .env
+echo DATABASE=$DATABASE >> .env
+echo DATABASE_PASSWORD=$DATABASE_PASSWORD >> .env
+echo DATABASE_LOCAL=$DATABASE_LOCAL >> .env
+echo DATABASE_TEST=$DATABASE_TEST >> .env
+echo JWT_SECRET=$JWT_SECRET >> .env
+echo JWT_EXPIRES_IN=$JWT_EXPIRES_IN >> .env
+echo JWT_COOKIE_EXPIRES_IN=$JWT_COOKIE_EXPIRES_IN >> .env
+echo JWT_ISSUER=$JWT_ISSUER >> .env
+echo EMAIL_USERNAME=$EMAIL_USERNAME >> .env
+echo EMAIL_PASSWORD=$EMAIL_PASSWORD >> .env
+echo EMAIL_HOST=$EMAIL_HOST >> .env
+echo EMAIL_PORT=$EMAIL_PORT >> .env
+
+echo 'securely copy .env file ...'
+scp sshenv alvin@206.189.196.15:~/
