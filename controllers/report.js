f528992158ed90c52cc5c18d6104b1bec65b9328
Alvin Abaho <abahoalvin@gmail.com>
2020-09-15 19:31:31 +0300

    populates program name and department & school id's in Get All report

--- controllers/report.js
+++ controllers/report.js
@@ -1,12 +1,17 @@
 const Report = require('../models/reports');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const { populate } = require('../models/reports');
 
 module.exports = {
   getAllReports: catchAsync(async (req, res, next) => {
     const reports = await Report.find({}).populate({
       path: 'student',
-      select: 'firstName lastName'
+      select: 'firstName lastName',
+      populate: [
+        { path: 'program', select: 'name -_id' },
+        { path: 'department', select: '-name -__v' }
+      ]
     });
 
     res.status(200).json(reports);
