089dd08d7c05896560b77bc59bc9c9072aa7fee1
CalvinKaregyeya <calvinkaregyeya@gmail.com>
2021-02-07 04:00:38 +0300

    adds report endpoints from the student module

--- routes/report.js
+++ routes/report.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const controller = require('../controllers/report');
+const studentAuth = require('../auth/studentAuth');
 const staffAuth = require('../auth/staffAuth');
 const AuthProtector = require('../auth/authProtector');
 
 const router = express.Router();
 
 router.use(AuthProtector());
-router.use(staffAuth.restrictTo('admin', 'principal', 'dean'));
-router.route('/').get(controller.getAllReports);
+
+router
+  .route('/student')
+  .all(studentAuth.restrictTo('student'))
+  .get(studentAuth.getMe(), controller.getReport)
+  .patch(studentAuth.getMe(), controller.updateReport);
+
+router.patch(
+  '/submit',
+  studentAuth.restrictTo('student'),
+  studentAuth.getMe(),
+  controller.submitReport
+);
+
+router
+  .route('/')
+  .get(staffAuth.restrictTo('admin', 'principal', 'dean'), controller.getAllReports)
+  .post(studentAuth.restrictTo('student'), studentAuth.getMe(), controller.addReport);
 
 module.exports = router;
