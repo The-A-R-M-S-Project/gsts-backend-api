6018a38ebcb9aa5718ffa254812d51cb560f7b61
CalvinKaregyeya <calvinkaregyeya@gmail.com>
2021-02-07 05:41:56 +0300

    adds multer utility

---
+++ utils/multerStorage.js
@@ -0,0 +1,24 @@
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const awsS3 = require('./aws');
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const storage = multerS3({
+  s3: awsS3,
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  contentDisposition: 'inline',
+  bucket: process.env.DO_BUCKET,
+  metadata: function(req, file, cb) {
+    cb(null, { fieldName: file.fieldname });
+  },
+  key: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+module.exports = multer({ storage: storage });
