858925a82d2b88a95ea598209fbace156d4ca51b
CalvinKaregyeya <calvinkaregyeya@gmail.com>
2021-02-09 03:55:02 +0300

    adds viva router

---
+++ routes/viva.js
@@ -0,0 +1,8 @@
+const express = require('express');
+// const controller = require('../controllers/comments');
+// const AuthProtector = require('../auth/authProtector');
+// const staffAuth = require('../auth/staffAuth');
+
+const router = express.Router({ mergeParams: true });
+
+module.exports = router;
