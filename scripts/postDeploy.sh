2573fea8ef47d73248120dff78883bf14fc40b45
CalvinKaregyeya <calvinkaregyeya@gmail.com>
2021-02-27 14:15:28 +0300

    adds script command to install eslint peerDeps

--- scripts/postDeploy.sh
+++ scripts/postDeploy.sh
@@ -2,6 +2,10 @@
 echo 'set production environment'
 echo NODE_ENV=production >> .env
 echo 'installing Dependencies ...'
+echo 'Installing eslint peer dependencies...'
+/usr/bin/npx install-peerdeps --dev eslint-config-airbnb-base
+echo 'Installing other
+ dependencies...'
 /usr/bin/npm install
 echo 'starting server'
 /usr/bin/pm2 restart server
